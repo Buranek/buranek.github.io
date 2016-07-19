75cce2d5bb5442da15f384b711b294a0b87e5a61
Buranek <buranek@ya.ru>
2016-07-19 12:51:42 +0300

    Скрипт для слабовидящих

--- js/shbmnk.js
+++ js/shbmnk.js
@@ -1,8 +1,6 @@
-<script>
 $(document).ready(function(){
 if (window.console) console.log('ШБМНК.js v1 загрузилось!'); // Если активна консоль отладки браузера
     $("button").click(function(){
-        $("p", "h2", "h3", "h4").css({"font-size": "200%"});
+        $("body").css({"font-size": "175%"});
     });
 });
-</script>

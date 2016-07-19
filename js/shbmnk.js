f63904cfda92ab0b3f85c1fa86af184cd7fc97c1
Buranek <buranek@ya.ru>
2016-07-19 11:39:42 +0300

    Пробуем дальше

--- js/shbmnk.js
+++ js/shbmnk.js
@@ -1,7 +1,8 @@
 <script>
 $(document).ready(function(){
+if (window.console) console.log('ШБМНК.js v1 загрузилось!'); // Если активна консоль отладки браузера
     $("button").click(function(){
-        $("p").css({"font-size": "200%"});
+        $("p", "h2", "h3", "h4").css({"font-size": "200%"});
     });
 });
 </script>

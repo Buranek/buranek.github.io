18e55ab1834c3e96739155c37c6723cb9bcd299e
Buranek <buranek@ya.ru>
2016-07-25 01:35:06 +0300

    ТЫ ОБМЫЛОК ТЫ ПЛАКТИКОВЫЙ КУСОК САЙТА ТЫ С ШБМНК Я СТАРАЛСЯ ДЕЛАТЬ ТЕБЯ

--- js/shbmnk.js
+++ js/shbmnk.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-if (window.console) console.log('ШБМНК.js версии 1.4.4 загрузилось!'); // Если активна консоль отладки браузера, показать текст
+if (window.console) console.log("ШБМНК.js версии 1.4.4 загрузилось!"); // Если активна консоль отладки браузера, показать текст
 	// Режим окулиста
 	$("button-brown").click(function(){ // Если была нажата кнопка увеличения (исхитрожопился через css)
 		// Поменять размеры шрифтов, задействованных на главной странице сайта
@@ -8,15 +8,15 @@ if (window.console) console.log('ШБМНК.js версии 1.4.4 загрузи
 		$("h3").css({"font-size": "155%"});
 		$("h4").css({"font-size": "165%"});
 		// Поменять картинки
-		$('body').css('background-image', 'url(../images/theme-okulist/back.png)'); // Поменять общий фон на окулистический
-		$('#left').css('background-image', 'url(../images/theme-okulist/left-body.png)'); // Поменять левый фон на окулистический
-		$('#content').css('background', '#FFF'); // Поменять фон шапки на белый
-		$('#right').css('background', 'grey'); // Поменять фон правого блока на серый
-		$('#footer p').css('background-image', 'url(../images/theme-okulist/footerback.png)'); // Поменять подвальный фон на окулистический
-		$('newsbox').css('background-image', 'url(../images/theme-okulist/newsbox.png)'); // Поменять фон новостей на простой
-		$('#lesopark').attr('src','images/theme-okulist/lesopark.png'); //Поменять лесопарк на ЧБ
-		$('#borago').attr('src','images/theme-okulist/bogaro.png'); //Поменять бораго на простое
-		$('#FlagRF').attr('src','images/theme-okulist/FlagRF.png'); //Поменять флаг РФ тем, который без эффектов
+		$("body").css("background-image", "url(../images/theme-okulist/back.png)"); // Поменять общий фон на окулистический
+		$("#left").css("background-image", "url(../images/theme-okulist/left-body.png)"); // Поменять левый фон на окулистический
+		$("#content").css("background", "#FFF"); // Поменять фон шапки на белый
+		$("#right").css("background", "grey"); // Поменять фон правого блока на серый
+		$("#footer p").css("background-image", "url(../images/theme-okulist/footerback.png)"); // Поменять подвальный фон на окулистический
+		$(".newsbox").css("background-image", "url(../images/theme-okulist/newsbox.png)"); // Поменять фон новостей на простой
+		$("#lesopark").attr("src","images/theme-okulist/lesopark.png"); //Поменять лесопарк на ЧБ
+		$("#borago").attr("src","images/theme-okulist/bogaro.png"); //Поменять бораго на простое
+		$("#FlagRF").attr("src","images/theme-okulist/FlagRF.png"); //Поменять флаг РФ тем, который без эффектов
     });
     
 	//Режим НЕ-окулиста
@@ -28,14 +28,14 @@ if (window.console) console.log('ШБМНК.js версии 1.4.4 загрузи
 		$("h3").css({"font-size": "100%"});
 		$("h4").css({"font-size": "100%"});
 		// Поменять картинки
-		$('body').css('background-image', 'url(../images/theme/back.png)'); // Поменять общий фон на обычный
-		$('#left').css('background-image', 'url(../images/theme/left-body.png)'); // Поменять левый фон на обычный
-		$('#content').css('background', '#faf6f6'); // Поменять фон шапки на обычный
-		$('#right').css('background', '#ccc2ab'); // Поменять фон правого блока на обычный
-		$('#footer p').css('background-image', 'url(../images/theme/footerback.jpg)'); // Поменять подвальный фон на обычный
-		$('newsbox').css('background-image', 'url(../images/theme/newsbox.png)'); // Поменять фон новостей на обычный
-		$('#lesopark').attr('src','images/theme/lesopark.png'); //Поменять лесопарк на обычный
-		$('#borago').attr('src','images/theme/bogaro.gif'); //Поменять бораго на обычное
-		$('#FlagRF').attr('src','images/theme/FlagRF.png'); //Поменять флаг РФ тем, который с эффектами
+		$("body").css("background-image", "url(../images/theme/back.png)"); // Поменять общий фон на обычный
+		$("#left").css("background-image", "url(../images/theme/left-body.png)"); // Поменять левый фон на обычный
+		$("#content").css("background", "#faf6f6"); // Поменять фон шапки на обычный
+		$("#right").css("background", "#ccc2ab"); // Поменять фон правого блока на обычный
+		$("#footer p").css("background-image", "url(../images/theme/footerback.jpg)"); // Поменять подвальный фон на обычный
+		$("#newsbox").css("background-image", "url(../images/theme/newsbox.png)"); // Поменять фон новостей на обычный
+		$("#lesopark").attr("src","images/theme/lesopark.png"); //Поменять лесопарк на обычный
+		$("#borago").attr("src","images/theme/bogaro.gif"); //Поменять бораго на обычное
+		$("#FlagRF").attr("src","images/theme/FlagRF.png"); //Поменять флаг РФ тем, который с эффектами
     });
 });

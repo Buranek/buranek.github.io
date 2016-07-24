4f776f72f290ba80aa199910d756273e3663cb78
Buranek <buranek@ya.ru>
2016-07-25 00:06:27 +0300

    Потом доделаю

--- js/shbmnk.js
+++ js/shbmnk.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 if (window.console) console.log('ШБМНК.js v1.3 загрузилось!'); // Если активна консоль отладки браузера, показать текст
-    $("button").click(function(){ // Если была нажата кнопка
-        $("body").css({"font-size": "175%"});
-        $("h2").css({"font-size": "150%"});
-        $("h3").css({"font-size": "155%"});
-        $("h4").css({"font-size": "165%"});
-        $('body').css('background-image', 'url(../images/theme-okulist/back.png)'); // Поменять фон на окулистический
-        $('lesopark').src = "../images/theme-okulist/lesopark.png"; // Поменять лесопарк на ЧБ
-        $('borago').src = "../images/theme-okulist/borago.gif"; // Поменять бораго на простое
+	$("button").click(function(){ // Если была нажата кнопка
+		// Поменять размеры шрифтов, задействованных на главной странице сайта
+		$("body").css({"font-size": "175%"});
+		$("h2").css({"font-size": "150%"});
+		$("h3").css({"font-size": "155%"});
+		$("h4").css({"font-size": "165%"});
+		$('body').css('background-image', 'url(../images/theme-okulist/back.png)'); // Поменять фон на окулистический
+        //$('lesopark').src = "../images/theme-okulist/lesopark.png"; // Поменять лесопарк на ЧБ (НЕ РАБОТАЕТ)
+        //$('borago').src = "../images/theme-okulist/borago.gif"; // Поменять бораго на простое (НЕ РАБОТАЕТ)
     });
 });

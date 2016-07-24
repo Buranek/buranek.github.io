a8b73b4f4d027b77702eddbffba86423112c0bfc
Buranek <buranek@ya.ru>
2016-07-25 00:33:51 +0300

    ШБ лучше

--- js/shbmnk.js
+++ js/shbmnk.js
@@ -11,6 +11,7 @@ if (window.console) console.log('ШБМНК.js версии 1.4 загрузил
 		// Поменять картинки
 		$('body').css('background-image', 'url(../images/theme-okulist/back.png)'); // Поменять общий фон на окулистический
 		$('#left').css('background-image', 'url(../images/theme-okulist/left-body.png)'); // Поменять левый фон на окулистический
+		$('#right').css('background', '#FFF'); // Поменять фон правого блока
 		$('#footer p').css('background-image', 'url(../images/theme-okulist/footerback.png)'); // Поменять подвальный фон на окулистический
 		$('#lesopark').attr('src','images/theme-okulist/lesopark.png'); //Поменять лесопарк на ЧБ
 		$('#borago').attr('src','images/theme-okulist/bogaro.png'); //Поменять бораго на простое
@@ -18,17 +19,17 @@ if (window.console) console.log('ШБМНК.js версии 1.4 загрузил
     
     // Режим НЕ-окулиста
     $("button").click(function(){ // Если была нажата кнопка
-		var id = $(this).attr('go-bigger'); // Если была нажата кнопка увеличения
+		var id = $(this).attr('go-small'); // Если была нажата кнопка уменьшения
 		// Поменять размеры шрифтов, задействованных на главной странице сайта
 		$("body").css({"font-size": "100%"});
 		$("h2").css({"font-size": "100%"});
 		$("h3").css({"font-size": "100%"});
 		$("h4").css({"font-size": "100%"});
 		// Поменять картинки
-		$('body').css('background-image', 'url(../images/theme/back.png)'); // Поменять общий фон на окулистический
-		$('#left').css('background-image', 'url(../images/theme/left-body.png)'); // Поменять левый фон на окулистический
-		$('#footer p').css('background-image', 'url(../images/theme/footerback.jpg)'); // Поменять подвальный фон на окулистический
-		$('#lesopark').attr('src','images/theme/lesopark.png'); //Поменять лесопарк на ЧБ
-		$('#borago').attr('src','images/theme-okulist/bogaro.gif'); //Поменять бораго на простое
+		$('body').css('background-image', 'url(../images/theme/back.png)'); // Поменять общий фон на обычный
+		$('#left').css('background-image', 'url(../images/theme/left-body.png)'); // Поменять левый фон на обычный
+		$('#footer p').css('background-image', 'url(../images/theme/footerback.jpg)'); // Поменять подвальный фон на обычный
+		$('#lesopark').attr('src','images/theme/lesopark.png'); //Поменять лесопарк на обычный
+		$('#borago').attr('src','images/theme-okulist/bogaro.gif'); //Поменять бораго на обычное
     });
 });

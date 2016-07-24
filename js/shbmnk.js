5a16b35c013b60acf59cffb067a44bcae88c1a80
Buranek <buranek@ya.ru>
2016-07-25 01:18:16 +0300

    Больше ШБМНК

--- js/shbmnk.js
+++ js/shbmnk.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-if (window.console) console.log('ШБМНК.js версии 1.4.2 загрузилось!'); // Если активна консоль отладки браузера, показать текст
+if (window.console) console.log('ШБМНК.js версии 1.4.3 загрузилось!'); // Если активна консоль отладки браузера, показать текст
 	// Режим окулиста
 	$("button-brown").click(function(){ // Если была нажата кнопка увеличения (исхитрожопился через css)
 		// Поменять размеры шрифтов, задействованных на главной странице сайта
@@ -13,8 +13,10 @@ if (window.console) console.log('ШБМНК.js версии 1.4.2 загрузи
 		$('#content').css('background', '#FFF'); // Поменять фон шапки на белый
 		$('#right').css('background', '#FFF'); // Поменять фон правого блока на белый
 		$('#footer p').css('background-image', 'url(../images/theme-okulist/footerback.png)'); // Поменять подвальный фон на окулистический
+		$('.newsbox').css('background-image', 'url(../images/theme-okulist/newsbox.png)'); // Поменять фон новостей на простой
 		$('#lesopark').attr('src','images/theme-okulist/lesopark.png'); //Поменять лесопарк на ЧБ
 		$('#borago').attr('src','images/theme-okulist/bogaro.png'); //Поменять бораго на простое
+		$('#FlagRF').attr('src','images/theme-okulist/FlagRF.png'); //Поменять флаг РФ тем, который без эффектов
     });
     
 	//Режим НЕ-окулиста
@@ -31,7 +33,9 @@ if (window.console) console.log('ШБМНК.js версии 1.4.2 загрузи
 		$('#content').css('background', '#faf6f6'); // Поменять фон шапки на обычный
 		$('#right').css('background', '#ccc2ab'); // Поменять фон правого блока на обычный
 		$('#footer p').css('background-image', 'url(../images/theme/footerback.jpg)'); // Поменять подвальный фон на обычный
+		$('.newsbox').css('background-image', 'url(../images/theme/newsbox.png)'); // Поменять фон новостей на обычный
 		$('#lesopark').attr('src','images/theme/lesopark.png'); //Поменять лесопарк на обычный
 		$('#borago').attr('src','images/theme/bogaro.gif'); //Поменять бораго на обычное
+		$('#FlagRF').attr('src','images/theme/FlagRF.png'); //Поменять флаг РФ тем, который с эффектами
     });
 });

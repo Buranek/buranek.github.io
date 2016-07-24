9775443e2c47cc8571feabe59f35acfda11e610b
Buranek <buranek@ya.ru>
2016-07-25 00:31:13 +0300

    О как!

--- js/shbmnk.js
+++ js/shbmnk.js
@@ -1,20 +1,34 @@
 $(document).ready(function(){
-if (window.console) console.log('ШБМНК.js v1.3 загрузилось!'); // Если активна консоль отладки браузера, показать текст
+if (window.console) console.log('ШБМНК.js версии 1.4 загрузилось!'); // Если активна консоль отладки браузера, показать текст
 	// Режим окулиста
 	$("button").click(function(){ // Если была нажата кнопка
-		alert(this.go-bigger);
+		var id = $(this).attr('go-bigger'); // Если была нажата кнопка увеличения
 		// Поменять размеры шрифтов, задействованных на главной странице сайта
 		$("body").css({"font-size": "175%"});
 		$("h2").css({"font-size": "150%"});
 		$("h3").css({"font-size": "155%"});
 		$("h4").css({"font-size": "165%"});
+		// Поменять картинки
 		$('body').css('background-image', 'url(../images/theme-okulist/back.png)'); // Поменять общий фон на окулистический
 		$('#left').css('background-image', 'url(../images/theme-okulist/left-body.png)'); // Поменять левый фон на окулистический
-		$('#footer p').css('background-image', 'url(../images/theme-okulist/footerback.jpg)'); // Поменять подвальный фон на окулистический
-		$('#lesopark').attr('src','images/theme-okulist/lesopark.png'); //Поменять лесопарк на ЧБ (НЕ РАБОТАЕТ)
-		$('#borago').attr('src','images/theme-okulist/bogaro.png'); //Поменять бораго на простое (НЕ РАБОТАЕТ)
-		
-		
-        //$('borago').src = "../images/theme-okulist/borago.gif"; // Поменять бораго на простое (НЕ РАБОТАЕТ)
+		$('#footer p').css('background-image', 'url(../images/theme-okulist/footerback.png)'); // Поменять подвальный фон на окулистический
+		$('#lesopark').attr('src','images/theme-okulist/lesopark.png'); //Поменять лесопарк на ЧБ
+		$('#borago').attr('src','images/theme-okulist/bogaro.png'); //Поменять бораго на простое
+    });
+    
+    // Режим НЕ-окулиста
+    $("button").click(function(){ // Если была нажата кнопка
+		var id = $(this).attr('go-bigger'); // Если была нажата кнопка увеличения
+		// Поменять размеры шрифтов, задействованных на главной странице сайта
+		$("body").css({"font-size": "100%"});
+		$("h2").css({"font-size": "100%"});
+		$("h3").css({"font-size": "100%"});
+		$("h4").css({"font-size": "100%"});
+		// Поменять картинки
+		$('body').css('background-image', 'url(../images/theme/back.png)'); // Поменять общий фон на окулистический
+		$('#left').css('background-image', 'url(../images/theme/left-body.png)'); // Поменять левый фон на окулистический
+		$('#footer p').css('background-image', 'url(../images/theme/footerback.jpg)'); // Поменять подвальный фон на окулистический
+		$('#lesopark').attr('src','images/theme/lesopark.png'); //Поменять лесопарк на ЧБ
+		$('#borago').attr('src','images/theme-okulist/bogaro.gif'); //Поменять бораго на простое
     });
 });

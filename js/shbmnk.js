f07c97718bb13d03306e26945ec1809cc58845e2
Buranek <buranek@ya.ru>
2016-07-25 00:54:34 +0300

    Должно работать

--- js/shbmnk.js
+++ js/shbmnk.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-if (window.console) console.log('ШБМНК.js версии 1.4 загрузилось!'); // Если активна консоль отладки браузера, показать текст
+if (window.console) console.log('ШБМНК.js версии 1.4.1 загрузилось!'); // Если активна консоль отладки браузера, показать текст
 	// Режим окулиста
-	$("button-brown").click(function(){ // Если была нажата кнопка
-		//var id = $(this).attr('go-bigger'); // Если была нажата кнопка увеличения
+	$("button-brown").click(function(){ // Если была нажата кнопка увеличения (исхитрожопился через css)
 		// Поменять размеры шрифтов, задействованных на главной странице сайта
 		$("body").css({"font-size": "175%"});
 		$("h2").css({"font-size": "150%"});
@@ -17,19 +16,19 @@ if (window.console) console.log('ШБМНК.js версии 1.4 загрузил
 		$('#borago').attr('src','images/theme-okulist/bogaro.png'); //Поменять бораго на простое
     });
     
-    //// Режим НЕ-окулиста
-    //$("button id=go small").click(function(){ // Если была нажата кнопка
+//Режим НЕ-окулиста
+    $("button-green").click(function(){ // Если была нажата кнопка уменьшения (исхитрожопился через css)
 		
-		//// Поменять размеры шрифтов, задействованных на главной странице сайта
-		//$("body").css({"font-size": "100%"});
-		//$("h2").css({"font-size": "100%"});
-		//$("h3").css({"font-size": "100%"});
-		//$("h4").css({"font-size": "100%"});
-		//// Поменять картинки
-		//$('body').css('background-image', 'url(../images/theme/back.png)'); // Поменять общий фон на обычный
-		//$('#left').css('background-image', 'url(../images/theme/left-body.png)'); // Поменять левый фон на обычный
-		//$('#footer p').css('background-image', 'url(../images/theme/footerback.jpg)'); // Поменять подвальный фон на обычный
-		//$('#lesopark').attr('src','images/theme/lesopark.png'); //Поменять лесопарк на обычный
-		//$('#borago').attr('src','images/theme-okulist/bogaro.gif'); //Поменять бораго на обычное
-    //});
+		// Поменять размеры шрифтов, задействованных на главной странице сайта
+		$("body").css({"font-size": "100%"});
+		$("h2").css({"font-size": "100%"});
+		$("h3").css({"font-size": "100%"});
+		$("h4").css({"font-size": "100%"});
+		// Поменять картинки
+		$('body').css('background-image', 'url(../images/theme/back.png)'); // Поменять общий фон на обычный
+		$('#left').css('background-image', 'url(../images/theme/left-body.png)'); // Поменять левый фон на обычный
+		$('#footer p').css('background-image', 'url(../images/theme/footerback.jpg)'); // Поменять подвальный фон на обычный
+		$('#lesopark').attr('src','images/theme/lesopark.png'); //Поменять лесопарк на обычный
+		$('#borago').attr('src','images/theme-okulist/bogaro.gif'); //Поменять бораго на обычное
+    });
 });

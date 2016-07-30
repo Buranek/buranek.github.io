50c04a717d209c885efaa7e60ada9b985c5879ba
Buranek <buranek@ya.ru>
2016-07-30 23:04:04 +0300

    Новинки

--- js/shbmnk.js
+++ js/shbmnk.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 if (window.console) console.log("ШБМНК.js версии 1.4.4 загрузилось!"); // Если активна консоль отладки браузера, показать текст
-	// Режим окулиста
+	// Режим окулиста (пока что только для главной страницы)
 	$("button-brown").click(function(){ // Если была нажата кнопка увеличения (исхитрожопился через css)
-		// Поменять размеры шрифтов, задействованных на главной странице сайта
+		// Поменять размеры шрифтов, задействованных на главной странице сайта на крупные
 		$("body").css({"font-size": "175%"});
 		$("h2").css({"font-size": "150%"});
 		$("h3").css({"font-size": "155%"});
@@ -15,14 +15,14 @@ if (window.console) console.log("ШБМНК.js версии 1.4.4 загрузи
 		$("#footer p").css("background-image", "url(../images/theme-okulist/footerback.png)"); // Поменять подвальный фон на окулистический
 		$(".newsbox").css("background-image", "url(../images/theme-okulist/newsbox.png)"); // Поменять фон новостей на простой
 		$("#lesopark").attr("src","images/theme-okulist/lesopark.png"); //Поменять лесопарк на ЧБ
-		$("#borago").attr("src","images/theme-okulist/bogaro.png"); //Поменять бораго на простое
-		$("#FlagRF").attr("src","images/theme-okulist/FlagRF.png"); //Поменять флаг РФ тем, который без эффектов
+		$("#borago").attr("src","images/theme-okulist/bogaro.png"); // Поменять бораго на простое
+		$("#FlagRF").attr("src","images/theme-okulist/FlagRF.png"); // Поменять флаг РФ тем, который без эффектов
     });
     
-	//Режим НЕ-окулиста
+	// Режим НЕ-окулиста (пока что только для главной страницы)
     $("button-green").click(function(){ // Если была нажата кнопка уменьшения (исхитрожопился через css)
 		
-		// Поменять размеры шрифтов, задействованных на главной странице сайта
+		// Поменять размеры шрифтов, задействованных на главной странице сайта на обычные
 		$("body").css({"font-size": "100%"});
 		$("h2").css({"font-size": "100%"});
 		$("h3").css({"font-size": "100%"});
@@ -35,7 +35,7 @@ if (window.console) console.log("ШБМНК.js версии 1.4.4 загрузи
 		$("#footer p").css("background-image", "url(../images/theme/footerback.jpg)"); // Поменять подвальный фон на обычный
 		$("#newsbox").css("background-image", "url(../images/theme/newsbox.png)"); // Поменять фон новостей на обычный
 		$("#lesopark").attr("src","images/theme/lesopark.png"); //Поменять лесопарк на обычный
-		$("#borago").attr("src","images/theme/bogaro.gif"); //Поменять бораго на обычное
-		$("#FlagRF").attr("src","images/theme/FlagRF.png"); //Поменять флаг РФ тем, который с эффектами
+		$("#borago").attr("src","images/theme/bogaro.gif"); // Поменять бораго на обычное
+		$("#FlagRF").attr("src","images/theme/FlagRF.png"); // Поменять флаг РФ тем, который с эффектами
     });
 });
